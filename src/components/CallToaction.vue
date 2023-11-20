f1889673c22aabb9f62a1852ab3ceeb6ffb7535d
ehab-hassan <hassane@codescalers.com>
2023-11-20 14:04:16 +0200

    about second section updated

---
+++ src/components/CallToaction.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    v-for="(calltoactio, i) in calltoactions"
+    :key="i"
+    class="my-5 text-center container mx-auto lg:my-24 lg:max-w-6xl"
+  >
+    <h2
+      class="text-2xl md:text-3xl lg:text-5xl mb-3 leading-none font-bold tracking-tight text-blue-800"
+    >
+      {{ calltoactio.title }}
+    </h2>
+    <p
+      class="my-3 text-base mx-3 lg:text-xl font-medium leading-6 lg:leading-8 text-blue-800"
+      v-html="calltoactio.content"
+    ></p>
+    <p
+      class="my-3 text-base mx-3 lg:text-xl font-medium leading-6 lg:leading-8 text-blue-800"
+      v-html="calltoactio.content2"
+    ></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CallToaction",
+  props: ["calltoactions"],
+};
+</script>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,15 +1,18 @@
 <template>
   <AboutHeader :items="HeaderView" />
+  <CallToaction :calltoactions="CallToactionitems" />
   <BackGround :backgroundImg="backgroundItems" />
 </template>
 
 <script>
 import AboutHeader from "@/components/AboutHeader.vue";
+import CallToaction from "@/components/CallToaction.vue";
 import BackGround from "@/components/BackGround.vue";
 export default {
   name: "AboutUs",
   components: {
     AboutHeader,
+    CallToaction,
     BackGround,
   },
 
@@ -30,6 +33,15 @@ export default {
           logo: "logo",
           title: "title",
           content: "content",
+        },
+      ],
+      CallToactionitems: [
+        {
+          title: "About ZICTIA",
+          content:
+            "Zanzibar ICT Infrastructure Agency (ZICTIA) is a government agency established in 2018, responsible for managing Zanzibar's National Fiber backbone and Data Centre. In 2020, an act was enacted, expanding its mandate to include managing ICT infrastructure, telecommunication towers, and providing affordable, secure, and convergent ICT infrastructure services to the public",
+          content2:
+            "ZICTIA has a dedicated team of technical experts available 24/7 for support and education on ICT infrastructure. They also offer a secure Data Centre for public and private institutions for data backup and disaster recovery. ZICTIA aims to advance Information and Communication Technology in the country through its services.",
         },
       ],
     };
