32c35b38d0db706a7db51d64a71f1a6d78a576a0
ehab-hassan <hassane@codescalers.com>
2023-11-20 13:11:27 +0200

    update about page

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>
-    About Us
-  </div>
+  <AboutHeader :items="HeaderView" />
+  <BackGround :backgroundImg="backgroundItems" />
 </template>
+
+<script>
+import AboutHeader from "@/components/AboutHeader.vue";
+import BackGround from "@/components/BackGround.vue";
+export default {
+  name: "AboutUs",
+  components: {
+    AboutHeader,
+    BackGround,
+  },
+
+  data() {
+    return {
+      HeaderView: [
+        {
+          bg: "home/about_header.png",
+          logo: require("../assets/imgs/home/logo_white.png"),
+          title: "Elevating Zanzibar's Connectivity",
+          content:
+            "Explore our story, mission, and the impactful role we play in shaping a digitally empowered future for all.",
+        },
+      ],
+      backgroundItems: [
+        {
+          bg: "home/about_bottom.png",
+          logo: "logo",
+          title: "title",
+          content: "content",
+        },
+      ],
+    };
+  },
+};
+</script>
