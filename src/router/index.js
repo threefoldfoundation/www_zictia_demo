9357ff3438534576d85f0ae1d22c8d060ef8438a
samaradel <samar@incubaid.com>
2023-11-23 13:26:20 +0200

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
-import { createRouter } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
