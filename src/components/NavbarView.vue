c0e6de42c0a4966de5e34856034d3023aec6c517
samaradel <samar@incubaid.com>
2023-11-23 13:38:00 +0200

    make navbar dynamic

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -28,303 +28,71 @@
         <div
           class="absolute right-0 flex items-center justify-center sm:items-stretch sm:justify-start pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         >
-          <div class="hidden sm:ml-6 sm:flex sm:space-x-4">
+          <ul class="hidden sm:ml-6 sm:flex sm:space-x-4">
             <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-            <router-link
-              to="/"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >Home</router-link
+            <li
+              v-for="(item, index) in navItems"
+              :key="index"
+              class="inline-flex items-center border-b-2 border-transparent hover:border-gray-300"
             >
-            <router-link
-              to="/about"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >About Us</router-link
-            >
-            <Menu
-              as="div"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-            >
-              <div>
-                <MenuButton
-                  class="inline-flex w-full justify-center gap-x-1.5 bg-white text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                >
-                  Management
-                  <ChevronDownIcon
-                    class="-mr-1 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </MenuButton>
-              </div>
-
-              <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
+              <router-link
+                v-if="!item.children"
+                :to="item.path"
+                class="px-1 pt-1 text-sm font-semibold text-gray-500 hover:text-gray-700"
+                >{{ item.name }}</router-link
               >
-                <MenuItems
-                  class="absolute top-12 z-10 mt-2 w-56 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                >
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/organization_structure"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                      >
-                        Organization Structure
-                      </router-link>
-                    </MenuItem>
-                  </div>
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/team_members"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >Team Members</router-link
-                      >
-                    </MenuItem>
-                  </div>
-                </MenuItems>
-              </transition>
-            </Menu>
-
-            <Menu
-              as="div"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-            >
-              <div>
-                <MenuButton
-                  class="inline-flex w-full justify-center gap-x-1.5 bg-white text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                >
-                  Our Services
-                  <ChevronDownIcon
-                    class="-mr-1 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </MenuButton>
-              </div>
-
-              <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
+              <Menu
+                v-else
+                as="div"
+                class="px-1 pt-1 text-sm font-semibold text-gray-500 hover:text-gray-700"
               >
-                <MenuItems
-                  class="absolute top-12 z-10 mt-2 w-56 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                >
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/vpn"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                      >
-                        VPN
-                      </router-link>
-                    </MenuItem>
-                  </div>
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/ipmpls"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >IP MPLS</router-link
-                      >
-                    </MenuItem>
-                  </div>
+                <div>
+                  <MenuButton
+                    class="inline-flex w-full justify-center gap-x-1.5 bg-white text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  >
+                    {{ item.name }}
+                    <ChevronDownIcon
+                      class="-mr-1 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </MenuButton>
+                </div>
 
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="colocation"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >CO-Location</router-link
-                      >
-                    </MenuItem>
-                  </div>
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/virtualmachine"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >Virtual Machine</router-link
-                      >
-                    </MenuItem>
-                  </div>
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/fiberLeaseServices"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >Capacity Lease</router-link
-                      >
-                    </MenuItem>
-                  </div>
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/internetservices"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >Internet Services</router-link
-                      >
-                    </MenuItem>
-                  </div>
-                </MenuItems>
-              </transition>
-            </Menu>
-            <router-link
-              to="/fiberRoutes"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >Fiber Routes</router-link
-            >
-            <router-link
-              to="/softnet_news"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >News &amp; Events</router-link
-            >
-            <Menu
-              as="div"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-            >
-              <div>
-                <MenuButton
-                  class="inline-flex w-full justify-center gap-x-1.5 bg-white text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                <transition
+                  enter-active-class="transition ease-out duration-100"
+                  enter-from-class="transform opacity-0 scale-95"
+                  enter-to-class="transform opacity-100 scale-100"
+                  leave-active-class="transition ease-in duration-75"
+                  leave-from-class="transform opacity-100 scale-100"
+                  leave-to-class="transform opacity-0 scale-95"
                 >
-                  Documents
-                  <ChevronDownIcon
-                    class="-mr-1 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </MenuButton>
-              </div>
-
-              <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
-              >
-                <MenuItems
-                  class="absolute top-12 z-10 mt-2 w-56 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                >
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/tender"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
+                  <MenuItems
+                    class="absolute top-12 z-10 mt-2 w-56 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  >
+                    <div class="py-1">
+                      <MenuItem
+                        v-slot="{ active }"
+                        v-for="(element, id) in item.children"
+                        :key="id"
                       >
-                        Tender
-                      </router-link>
-                    </MenuItem>
-                  </div>
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/legal_act"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >Legalact</router-link
-                      >
-                    </MenuItem>
-                  </div>
-
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/bronchure"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >Borchure</router-link
-                      >
-                    </MenuItem>
-                  </div>
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <router-link
-                        to="/form"
-                        :class="[
-                          active
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
-                        ]"
-                        >Form</router-link
-                      >
-                    </MenuItem>
-                  </div>
-                </MenuItems>
-              </transition>
-            </Menu>
-            <router-link
-              to="/faq"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >FAQ's</router-link
-            >
-            <router-link
-              to="/contact"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >Contact</router-link
-            >
-          </div>
+                        <router-link
+                          :to="element.path"
+                          :class="[
+                            active
+                              ? 'bg-gray-100 text-gray-900'
+                              : 'text-gray-700',
+                            'block px-4 py-2 text-sm',
+                          ]"
+                        >
+                          {{ element.name }}
+                        </router-link>
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </transition>
+              </Menu>
+            </li>
+          </ul>
           <!-- <button
             type="button"
             class="relative rounded-full bg-white pl-3 pt-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
@@ -563,10 +331,9 @@ export default {
   },
 };
 </script>
-<!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
 
 <style scoped>
-.router-link-active {
+li:has(> a.router-link-active) {
   color: #111827;
   border-color: #6366f1;
 }
