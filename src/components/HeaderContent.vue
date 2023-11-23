6c638a5920f49efc6e6f733b5edf9bfb64cd8aa5
samaradel <samar@incubaid.com>
2023-11-23 17:33:25 +0200

    Fix organization

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -31,7 +31,7 @@
             <div class="flex flex-wrap">
               <div class="w-full md:w-auto py-1 md:py-0 md:mr-4">
                 <button
-                  v-if="headerContent.button"
+                  v-if="headerContent.button && headerContent.link"
                   type="button"
                   class="mt-2 rounded-md bg-blue-600 px-4 py-1.5 lg:text-lg text-base font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                 >
