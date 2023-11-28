067ac39cd240cb7c066b6a43ab7494d36c15276e
ehab-hassan <hassane@codescalers.com>
2023-11-28 18:42:53 +0200

    updated fiberRoutes page

--- src/components/CallToactionBlue.vue
+++ src/components/CallToactionBlue.vue
@@ -3,7 +3,7 @@
     <div
       v-for="(calltoactionblue, i) in calltoactionblues"
       :key="i"
-      class="text-white sm:mx-3 text-center container mx-auto lg:max-w-6xl"
+      class="text-white text-center container mx-auto lg:max-w-6xl"
     >
       <p
         class="md:mb-12 mb-6 text-base mx-3 lg:text-lg font-medium leading-6 lg:leading-8"
@@ -18,7 +18,7 @@
 
       <iframe
         class="my-6 md:my-8"
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d38466.66579039967!2d39.210295!3d-6.199225!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185cdba0f57fac05%3A0xe6183b8532a857b5!2sZanzibar%20ICT%20Infrastructure%20Agency!5e1!3m2!1sen!2sus!4v1701179641781!5m2!1sen!2sus"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d507805.9206742061!2d39.051363792215874!3d-6.099187870905901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185d29602a2909e5%3A0xa035af4aad9b7d5f!2sUnguja!5e0!3m2!1sen!2seg!4v1701188843224!5m2!1sen!2seg"
         width="100%"
         height="450"
         style="border: 0"
@@ -26,13 +26,40 @@
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
+
+      <!-- <iframe
+        class="my-6 md:my-8"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d38466.66579039967!2d39.210295!3d-6.199225!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185cdba0f57fac05%3A0xe6183b8532a857b5!2sZanzibar%20ICT%20Infrastructure%20Agency!5e1!3m2!1sen!2sus!4v1701179641781!5m2!1sen!2sus"
+        width="100%"
+        height="450"
+        style="border: 0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe> -->
       <p
         class="my-3 text-base px-3 lg:text-lg font-medium leading-6 lg:leading-8"
         v-html="calltoactionblue.content"
       ></p>
       <p
-        class="my-3 text-base mx-3 lg:text-lg font-medium leading-6 lg:leading-8"
+        class="my-3 md:mb-12 text-base mx-3 lg:text-lg font-medium leading-6 lg:leading-8"
         v-html="calltoactionblue.content2"
+      ></p>
+
+      <iframe
+        class="my-6 md:my-8"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d302428.2257777368!2d39.59916800818977!3d-5.166842091024374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1842139f3f03aea3%3A0x616d9a539f11de85!2sPemba%20Island!5e0!3m2!1sen!2seg!4v1701188356646!5m2!1sen!2seg"
+        width="100%"
+        height="450"
+        style="border: 0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <p
+        class="my-3 text-base mx-3 lg:text-lg font-medium leading-6 lg:leading-8"
+        v-html="calltoactionblue.content4"
       ></p>
     </div>
   </div>
--- src/views/FiberRoutes.vue
+++ src/views/FiberRoutes.vue
@@ -40,6 +40,8 @@ export default {
             "In addition, we've expanded our network with a new fiber route, extending connectivity to previously underserved areas on Unguja Island, enabling more communities to access high-speed internet and advanced services. Complementing these routes are strategically placed Points of Presence (PoPs), creating convenient access points to our Fiber Routes Network, ensuring that cutting-edge technology is always within reach.",
           content3:
             "ZICTIA is at the forefront of revolutionizing connectivity across the beautiful islands of Zanzibar. Our state-of-the-art Fiber Routes Network spans the archipelago, connecting Unguja and Pemba Islands with an intricate web of high-capacity, resilient fiber optic infrastructure. With a focus on cutting-edge technology, robust maintenance, and wide-reaching connectivity, we're redefining how Zanzibar stays digitally connected.",
+          content4:
+            "ZICTIA's Fiber Routes Network is not just about connectivity; it's about transforming lives, powering businesses, and advancing Zanzibar's technological landscape. With a commitment to infrastructure excellence and innovative solutions, we're proud to lead the way in creating a connected Zanzibar.",
         },
       ],
       backgroundItems: [
