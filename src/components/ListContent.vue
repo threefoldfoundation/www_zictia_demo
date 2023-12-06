72fe1a965f6daa59c627bca49c4e5c2cfd539f1a
samaradel <samar@incubaid.com>
2023-12-06 16:55:16 +0200

    fix tenders

---
+++ src/components/ListContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="px-6 container lg:mx-auto py-10 mx-4 lg:py-32 lg:px-8">
+    <div class="">
+      <h2
+        class="text-2xl my-1 md:text-3xl lg:text-5xl leading-none font-bold tracking-tight text-blue-800"
+      >
+        TENDER
+      </h2>
+      <p class="text-xl leading-8 text-gray-800">Available tender</p>
+      <ul class="list-disc my-4">
+        <li
+          v-for="tender in tenders"
+          :key="tender.name"
+          class="font-semibold text-blue-800"
+        >
+          <a :href="tender.url" target="_blank">{{ tender.name }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListContent",
+  props: ["tenders"],
+};
+</script>
--- src/views/TenderPage.vue
+++ src/views/TenderPage.vue
@@ -1,12 +1,12 @@
 <template>
   <SmallBanner />
   <HeaderSection :items="HeaderView" />
-  <ListConent />
+  <ListContent :tenders="tenders" />
   <BackGround :backgroundImg="backgroundItems" />
 </template>
 
 <script>
-import ListConent from "@/components/ListConent.vue";
+import ListContent from "@/components/ListContent.vue";
 import SmallBanner from "@/components/SmallBanner.vue";
 import HeaderSection from "@/components/HeaderSection.vue";
 import BackGround from "@/components/BackGround.vue";
@@ -16,7 +16,7 @@ export default {
   components: {
     SmallBanner,
     HeaderSection,
-    ListConent,
+    ListContent,
     BackGround,
   },
 
@@ -37,6 +37,23 @@ export default {
           logo: "logo",
           title: "title",
           content: "content",
+        },
+      ],
+      tenders: [
+        {
+          name: "PUBLIC NOTICE.",
+          url: "./files/TENDER/TENDER.docx",
+        },
+        { name: "SUPPLY OF FIBER EQUIPMENT.", url: "#" },
+        {
+          name:
+            "SUPPLY OF WOODEN POLES AND ITS TENSION HOOKS IN A FRAMEWORK CONTRACT.",
+          url: "./files/TENDER/Supply of Wooden Poles.pdf",
+        },
+        {
+          name:
+            "GENERAL PROCUREMENT NOTICE [GPN] FOR GOODS, WORKS AND NONCONSULTANCY SERVICES FOR THE FINANCIAL YEAR 2021/2022",
+          url: "./files/TENDER/GPN.pdf",
         },
       ],
     };
