0082b228d488fe52e9d82e49304c90e9bcd046f3
ehab-hassan <hassane@codescalers.com>
2024-02-26 14:29:40 +0200

    update team

--- src/views/TeamMembers.vue
+++ src/views/TeamMembers.vue
@@ -17,32 +17,152 @@
       </div>
       <!-- text - end -->
 
-      <div
-        class="grid grid-cols-2 gap-x-4 gap-y-8 md:grid-cols-3 lg:gap-x-8 lg:gap-y-12"
-      >
-        <!-- person - start -->
-        <div v-for="member in members" :key="member.id">
+      <!-- general  -->
+      <div class="text-center mb-10">
+        <h3
+          class="text-2xl md:text-3xl mb-6 leading-none font-semibold tracking-tight text-white"
+        >
+          {{ generaltitle }}
+        </h3>
+        <div
+          class="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3 lg:gap-x-12 lg:gap-y-12"
+        >
+          <!-- person - start -->
           <div
-            class="mb-2 h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg sm:mb-4 sm:h-60 md:h-80"
+            class="col-auto md:col-start-2"
+            v-for="general in generals"
+            :key="general.id"
           >
-            <img
-              :src="require(`../assets/imgs/team/${member.image}`)"
-              loading="lazy"
-              :alt="member.name"
-              class="h-full w-full object-cover object-center"
-            />
-          </div>
-
-          <div>
-            <div class="font-bold text-white md:text-lg">
-              {{ member.name }}
+            <div
+              class="mb-2 h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg sm:mb-4 sm:h-60 md:h-80"
+            >
+              <img
+                :src="require(`../assets/imgs/team/${general.image}`)"
+                loading="lazy"
+                :alt="general.name"
+                class="h-full w-full object-cover object-center"
+              />
             </div>
-            <p class="mb-3 text-sm text-gray-200 md:mb-4 md:text-base">
-              {{ member.title }}
-            </p>
+
+            <div>
+              <div class="font-bold text-white md:text-lg">
+                {{ general.name }}
+              </div>
+              <p class="mb-3 text-sm text-gray-200 md:mb-4 md:text-base">
+                {{ general.title }}
+              </p>
+            </div>
           </div>
+          <!-- person - end -->
         </div>
-        <!-- person - end -->
+      </div>
+
+      <!-- directors  -->
+      <div class="text-center my-10">
+        <h3
+          class="text-2xl md:text-3xl mb-6 leading-none font-semibold tracking-tight text-white"
+        >
+          {{ directortitle }}
+        </h3>
+        <div
+          class="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3 lg:gap-x-12 lg:gap-y-12"
+        >
+          <!-- person - start -->
+          <div v-for="director in directors" :key="director.id">
+            <div
+              class="mb-2 h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg sm:mb-4 sm:h-60 md:h-80"
+            >
+              <img
+                :src="require(`../assets/imgs/team/${director.image}`)"
+                loading="lazy"
+                :alt="director.name"
+                class="h-full w-full object-cover object-center"
+              />
+            </div>
+
+            <div>
+              <div class="font-bold text-white md:text-lg">
+                {{ director.name }}
+              </div>
+              <p class="mb-3 text-sm text-gray-200 md:mb-4 md:text-base">
+                {{ director.title }}
+              </p>
+            </div>
+          </div>
+          <!-- person - end -->
+        </div>
+      </div>
+
+      <!-- units  -->
+      <div class="text-center my-10">
+        <h3
+          class="text-2xl md:text-3xl mb-6 leading-none font-semibold tracking-tight text-white"
+        >
+          {{ unittitle }}
+        </h3>
+        <div
+          class="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3 lg:gap-x-8 lg:gap-y-12"
+        >
+          <!-- person - start -->
+          <div v-for="unit in units" :key="unit.id">
+            <div
+              class="mb-2 h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg sm:mb-4 sm:h-60 md:h-80"
+            >
+              <img
+                :src="require(`../assets/imgs/team/${unit.image}`)"
+                loading="lazy"
+                :alt="unit.name"
+                class="h-full w-full object-cover object-center"
+              />
+            </div>
+
+            <div>
+              <div class="font-bold text-white md:text-lg">
+                {{ unit.name }}
+              </div>
+              <p class="mb-3 text-sm text-gray-200 md:mb-4 md:text-base">
+                {{ unit.title }}
+              </p>
+            </div>
+          </div>
+          <!-- person - end -->
+        </div>
+      </div>
+
+      <!-- divisions  -->
+      <div class="text-center my-10">
+        <h3
+          class="text-2xl md:text-3xl mb-6 leading-none font-semibold tracking-tight text-white"
+        >
+          {{ divisiontitle }}
+        </h3>
+        <div
+          class="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3 lg:gap-x-8 lg:gap-y-12"
+        >
+          <!-- person - start -->
+          <div v-for="division in divisions" :key="division.id">
+            <div
+              class="mb-2 h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg sm:mb-4 sm:h-60 md:h-80"
+            >
+              <img
+                :src="require(`../assets/imgs/team/${division.image}`)"
+                loading="lazy"
+                :alt="division.name"
+                class="h-full w-full object-cover object-center"
+              />
+            </div>
+
+            <div>
+              <div class="font-bold text-white md:text-lg">
+                {{ division.name }}
+              </div>
+              <p class="mb-3 text-sm text-gray-200 md:mb-4 md:text-base">
+                {{ division.title }}
+              </p>
+            </div>
+          </div>
+          <!-- person - end -->
+        </div>
       </div>
     </div>
   </div>
@@ -62,32 +182,43 @@ export default {
       title: "Team Members",
       description:
         "Comprising dedicated individuals driven by passion, we are on a shared journey to enhance the state of Tanzania and contribute positively to Tanzania's Internet infrastructure services, and improve the lives of those within our community.",
-      members: [
+      generaltitle: "Genaral Manager",
+      generals: [
         {
-          image: "5.png",
-          name: "Abdulrahman J Abdulrahman",
-          title: "Manager Fiber network",
+          image: "1.png",
+          name: "Shukuru A. Suleiman",
+          title: "Genaral Manager",
         },
+      ],
+      directortitle: "Department Directors",
+      directors: [
         {
-          image: "3.png",
-          name: "Abdulrahman M Abdulrahman",
-          title: "Manager Datacente",
+          image: "12.png",
+          name: "Saada A Mohammed",
+          title: "Director of Human resouce and Administration",
         },
-
         {
           image: "4.png",
           name: "Abubakar A Sheha",
           title: "Director Planning and Marketing",
         },
         {
+          image: "11.png",
+          name: "Mohammad A Msoma",
+          title: "Director of Technical and Innovation",
+        },
+      ],
+      unittitle: "Head Of Units",
+      units: [
+        {
+          image: "9.png",
+          name: "Maryam A Mtawa",
+          title: "Head of Internal Audit Unit",
+        },
+        {
           image: "2.png",
           name: "Ali S Mgambe",
           title: "Head of ICT Unit",
-        },
-        {
-          image: "6.png",
-          name: "Haji K Hamad",
-          title: "Manager Power and Cooling",
         },
         {
           image: "7.png",
@@ -100,9 +231,17 @@ export default {
           title: "Head of legal Unit",
         },
         {
-          image: "9.png",
-          name: "Maryam A Mtawa",
-          title: "Head of Internal Audit Unit",
+          image: "14.png",
+          name: "Soud S Said",
+          title: "Head of Accounting Unit",
+        },
+      ],
+      divisiontitle: "Head Of Division",
+      divisions: [
+        {
+          image: "13.png",
+          name: "Shani Kh Khamis",
+          title: "Manager Planning and Research",
         },
         {
           image: "10.png",
@@ -110,26 +249,22 @@ export default {
           title: "Manager of Operations",
         },
         {
-          image: "11.png",
-          name: "Mohammad A Msoma",
-          title: "Director of Technical and Innovation",
+          image: "5.png",
+          name: "Abdulrahman J Abdulrahman",
+          title: "Manager Fiber network",
         },
         {
-          image: "12.png",
-          name: "Saada A Mohammed",
-          title: "Director of Human resouce and Administration",
+          image: "3.png",
+          name: "Abdulrahman M Abdulrahman",
+          title: "Manager Datacente",
         },
         {
-          image: "13.png",
-          name: "Shani Kh Khamis",
-          title: "Manager Planning and Research",
-        },
-        {
-          image: "14.png",
-          name: "Soud S Said",
-          title: "Head of Accounting Unit",
+          image: "6.png",
+          name: "Haji K Hamad",
+          title: "Manager Power and Cooling",
         },
       ],
+
       backgroundItems: {
         bg: "team/team_bottom.png",
         logo: "logo",
