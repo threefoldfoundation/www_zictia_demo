a9429a68cf494477d09dc9e05246698aecb7fcf7
ehab-hassan <hassane@incubaid.com>
2024-02-27 12:38:51 +0200

    update footer

--- src/components/BackGround.vue
+++ src/components/BackGround.vue
@@ -1,5 +1,9 @@
 <template>
   <div v-if="backgroundImg.isBlue" class="bg-blue-950 py-4">
+
+    <h2 class="text-2xl text-center mt-6 md:text-3xl lg:text-5xl leading-none font-bold tracking-tight text-white">
+      Organization Structure
+    </h2>
     <img
       :src="backgroundImg.image"
       class="relative w-full py-4 sm:my-4 lg:w-1/2 mx-auto"
